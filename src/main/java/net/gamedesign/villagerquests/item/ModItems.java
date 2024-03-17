1c8c72331f1836bf14fa8f848b88e1cda22dfa0e
Valentin <valentin.istrate26@outlook.com>
2024-03-18 01:26:10 +0200

    added a quest item and a quest handler block

---
+++ src/main/java/net/gamedesign/villagerquests/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.gamedesign.villagerquests.item;
+
+import net.gamedesign.villagerquests.VillagerQuestsMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, VillagerQuestsMod.MOD_ID);
+
+    public static final RegistryObject<Item> QUEST = ITEMS.register("quest",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
